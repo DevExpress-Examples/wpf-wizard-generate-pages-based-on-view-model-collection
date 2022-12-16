3fec6b62b732251c5e9a401a0a7b1519515a58f8
DevExpressExampleBot <fakeEmail>
2022-12-16 22:18:29 +0400

    Source auto update [skip ci]

--- VB/DXSample/PageStyleSelector.vb
+++ VB/DXSample/PageStyleSelector.vb
@@ -1,17 +1,19 @@
-﻿Imports System.Windows
+Imports System.Windows
 Imports System.Windows.Controls
 
 Namespace DXSample
+
     Public Class PageStyleSelector
         Inherits StyleSelector
 
-        Public Property WelcomePageStyle() As Style
-        Public Property CommonPageStyle() As Style
-        Public Property CompletionPageStyle() As Style
+        Public Property WelcomePageStyle As Style
+
+        Public Property CommonPageStyle As Style
+
+        Public Property CompletionPageStyle As Style
+
         Public Overrides Function SelectStyle(ByVal item As Object, ByVal container As DependencyObject) As Style
-            If Not(TypeOf item Is PageViewModel) Then
-                Return Nothing
-            End If
+            If Not(TypeOf item Is PageViewModel) Then Return Nothing
             Dim vm = TryCast(item, PageViewModel)
             Select Case vm.Type
                 Case PageType.Welcome
@@ -21,6 +23,7 @@ Namespace DXSample
                 Case PageType.Completion
                     Return CompletionPageStyle
             End Select
+
             Return MyBase.SelectStyle(item, container)
         End Function
     End Class
