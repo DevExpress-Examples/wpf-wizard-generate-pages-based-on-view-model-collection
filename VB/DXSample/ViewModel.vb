7999f5a6291541ae0d3288fff46a249e20d920cc
DevExpressExampleBot <fakeEmail>
2023-03-28 13:09:19 +0400

    Source auto update [skip ci]

--- VB/DXSample/ViewModel.vb
+++ VB/DXSample/ViewModel.vb
@@ -1,17 +1,13 @@
-﻿Imports DevExpress.Mvvm
-Imports System
 Imports System.Collections.ObjectModel
-Imports System.Linq
 
 Namespace DXSample
+
     Public Class MainViewModel
-        Public Property Pages() As ObservableCollection(Of PageViewModel)
+
+        Public Property Pages As ObservableCollection(Of PageViewModel)
+
         Public Sub New()
-            Pages = New ObservableCollection(Of PageViewModel)() From { _
-                New PageViewModel() With {.Text = "Welcome Page", .Type = PageType.Welcome}, _
-                New PageViewModel() With {.Text = "Common Page", .Type = PageType.Common}, _
-                New PageViewModel() With {.Text = "Completion Page", .Type = PageType.Completion} _
-            }
+            Pages = New ObservableCollection(Of PageViewModel)() From {New PageViewModel() With {.Text = "Welcome Page", .Type = PageType.Welcome}, New PageViewModel() With {.Text = "Common Page", .Type = PageType.Common}, New PageViewModel() With {.Text = "Completion Page", .Type = PageType.Completion}}
         End Sub
     End Class
 
@@ -20,8 +16,11 @@ Namespace DXSample
         Common
         Completion
     End Enum
+
     Public Class PageViewModel
-        Public Property Text() As String
-        Public Property Type() As PageType
+
+        Public Property Text As String
+
+        Public Property Type As PageType
     End Class
 End Namespace
